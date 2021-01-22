1fe25989c3c23223931314d9b4cc7d8e56e5500e
freeman2333 <freeman23@bigmir.net>
2021-01-23 00:47:21 +0200

    commit

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,4 +1,4 @@
-import React,{useState, useRef, useEffect} from 'react'
+import React,{useState,  useEffect} from 'react'
 import PetItem from './PetItem';
 
 export const Page = () => {
@@ -8,37 +8,77 @@ export const Page = () => {
   const [petsArray, setPetsArray] = useState(getLocalStorage('pets'));
   const [foodsArray, setFoodsArray] = useState(getLocalStorage('food'));
   const [plantsArray, setPlantsArray] = useState(getLocalStorage('plants'));
+  const [unique, setUnique] = useState(false);
+  // const [adsAuthors, setAdsAuthors] = useState([]);
 
-  const getPetInfo = async (id) => {
+  const getUniqueValuesOneArray = (arr, category) => {
+    const ids = arr.map(item => item.id)
+    const uniqueIds = [...new Set(ids)]
+    const uniqueArray = uniqueIds.map(uniqueId => arr.find(item=>item.id === uniqueId))
+    switch (category) {
+      case ('pets'):
+        setPetsArray(uniqueArray)
+      case ('food'):
+        setFoodsArray(uniqueArray)
+      case ('plants'):
+        setPlantsArray(uniqueArray)
+      default:
+        return
+    }
+  }
+
+  const getUniqueValuesAllArrays = () => {
+    getUniqueValuesOneArray(petsArray,'pets')
+    getUniqueValuesOneArray(foodsArray,'food')
+    getUniqueValuesOneArray(plantsArray,'plants')
+    
+  }
+
+  const getCategory = async (e) => {
+    const id = e.target.dataset.click 
+    const categoryType = e.target.textContent
     const url = `https://run.mocky.io/v3/${id}`
     const data =  await fetch(url)
     const response = await data.json()
-    setPetsArray(response)
-    localStorage.setItem("pets", JSON.stringify(response));
+    if (categoryType.startsWith('PETS')) {
+      setPetsArray(response)
+      localStorage.setItem("pets", JSON.stringify(response));
+    }
+    else if (categoryType.startsWith('FOOD')) {
+      setFoodsArray(response)
+      localStorage.setItem("food", JSON.stringify(response));
+    }else if(categoryType.startsWith('PLANTS')){
+      setPlantsArray(response)
+      localStorage.setItem("plants", JSON.stringify(response));
+    }
   }
-  const getFoodInfo = async (id) => {
-    const url = `https://run.mocky.io/v3/${id}`
-    const data =  await fetch(url)
-    const response = await data.json()
-    setFoodsArray(response)
-    localStorage.setItem("food", JSON.stringify(response));
+
+  const handleCheckbox = () => {
+    setUnique(!unique)
+    
   }
-  const getPlantsInfo = async (id) => {
-    const url = `https://run.mocky.io/v3/${id}`
-    const data =  await fetch(url)
-    const response = await data.json()
-    setPlantsArray(response)
-    localStorage.setItem("plants", JSON.stringify(response));
-  }
+  useEffect(() => {
+    if (unique) {
+      getUniqueValuesAllArrays()
+    } else {
+      setPetsArray(getLocalStorage('pets'));
+      setFoodsArray(getLocalStorage('food'));
+      setPlantsArray(getLocalStorage('plants'));
+    }
+  }, [unique]);
 
   return (
     <main className="page-wrapper">
       <h1 className="campaign-title">
         Pets Campaign
-            </h1>
+      </h1>
+      <div className="checkbox-control">
+        <label htmlFor="unique">Unique items</label>
+        <input type="checkbox" name='unique' id='unique' checked={unique} onChange={handleCheckbox}/>
+      </div>
       <section className="campaigns">
         <div className="campaign-wrapper">
-          <span onClick={()=>getPetInfo("8afd1f5a-c999-4935-8291-412ea8756f1d")} className="button active" data-click="8afd1f5a-c999-4935-8291-412ea8756f1d">
+          <span onClick={getCategory} className="button active" data-click="8afd1f5a-c999-4935-8291-412ea8756f1d">
             PETS CAMPAIGN
           </span>
           
@@ -48,7 +88,7 @@ export const Page = () => {
           })}
         </div>
         <div className="campaign-wrapper">
-          <span onClick={()=>getFoodInfo("facf1e61-24ff-41c9-afc8-54837ab17327")} className="button" data-click="facf1e61-24ff-41c9-afc8-54837ab17327">
+          <span onClick={getCategory} className="button" data-click="facf1e61-24ff-41c9-afc8-54837ab17327">
             FOOD CAMPAIGN
                 </span>
           {foodsArray.map(pet => {
@@ -58,7 +98,7 @@ export const Page = () => {
           
         </div>
         <div className="campaign-wrapper">
-          <span onClick={()=>getPlantsInfo("f21695d8-64ec-40c0-9830-9310919533ec")} className="button" data-click="f21695d8-64ec-40c0-9830-9310919533ec">
+          <span onClick={getCategory} className="button" data-click="f21695d8-64ec-40c0-9830-9310919533ec">
             PLANTS CAMPAIGN
           </span>
           {plantsArray.map(pet => {
